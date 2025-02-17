2356c5747c5de6e3e6fa32b1186aa4f5185a8ec6
Channsoriya <broya.tk123@gmail.com>
2025-02-17 12:14:25 +0700

    udate user menu

--- resources/js/components/User/Menu.vue
+++ resources/js/components/User/Menu.vue
@@ -1,27 +1,31 @@
 <template>
-    <v-app-bar class="relative flex items-center justify-between flex-none px-6 py-3 m-0 transition-all fw-full">
+    <v-app-bar class="relativepx-6 py-3 m-0 transition-all fw-full ">
         <!-- Left Side (Logo) -->
         <div class="text-5xl font-bold">
             <span class="text-red-500">SR</span><span>NEWS</span>
         </div>
 
+
         <!-- Right Side (Navigation Menu) -->
-        <div class="flex items-center">
-            <v-menu v-for="(item, index) in items" :key="index" open-on-hover>
-                <template v-slot:activator="{ props }">
-                    <v-btn v-bind="props" variant="text" class="mx-2 text-lg">
-                        {{ item.item }}
-                        <v-icon right>mdi-chevron-down</v-icon>
-                    </v-btn>
-                </template>
-                <v-list>
-                    <v-list-item v-for="(sub, i) in item.subItems" :key="i" @click="handleClick(sub)"
-                        class="cursor-pointer">
-                        <v-list-item-title>{{ sub.item }}</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
-        </div>
+        <template v-slot:append>
+            <div class="flex items-center">
+                <v-menu v-for="(item, index) in items" :key="index" open-on-hover>
+                    <template v-slot:activator="{ props }">
+                        <v-btn v-bind="props" variant="text" class="mx-2 text-lg">
+                            {{ item.item }}
+                            <v-icon right>mdi-chevron-down</v-icon>
+                        </v-btn>
+                    </template>
+                    <v-list>
+                        <v-list-item v-for="(sub, i) in item.subItems" :key="i" @click="handleClick(sub)"
+                            class="cursor-pointer">
+                            <v-list-item-title>{{ sub.item }}</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+            </div>
+        </template>
+
     </v-app-bar>
 </template>
 
